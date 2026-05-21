502b66c1bcb2f9e81d60625d96b93308175abf4e
chloe donald <24106@burnside.school.nz>
2026-05-21 14:19:25 +1200

    player able to jump and double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,9 @@ const friction = 70
 
 const gravity = 120
 
+const max_jumps = 2
+var current_jumps = 1
+
 func _physics_process(delta: float) -> void:
 	var input_direction: Vector2 = input()
 	
@@ -18,6 +21,7 @@ func _physics_process(delta: float) -> void:
 		add_friction()
 		#play idle animation
 	player_movement()
+	jump()
 
 func input() -> Vector2:
 	var input_direction = Vector2.ZERO
@@ -34,3 +38,14 @@ func add_friction():
 
 func player_movement():
 	move_and_slide()
+
+func jump():
+	if Input.is_action_just_pressed("ui_up"):
+		if current_jumps < max_jumps:
+			velocity.y = jump_power
+			current_jumps += 1
+	else:
+		velocity.y += gravity
+	
+	if is_on_floor():
+		current_jumps = 1
