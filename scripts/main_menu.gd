7ec50f1dc10668a21af77b58a2f7cac2348de4a0
chloe donald <24106@burnside.school.nz>
2026-06-05 11:40:25 +1200

    creating the start of a basic main menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+# main section buttons
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_quit_game_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_select_level_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+# level select section buttons
+func _on_level_1_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_level_2_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+
+func _on_level_3_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
+
+func _on_back_button_pressed() -> void:
+	pass # Replace with function body.
