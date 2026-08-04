77f0a114745ed8dbfb4b5fb67b0d7e9931a9d0d6
24106 <24106@burnside.school.nz>
2026-08-04 16:36:41 +1200

    adding ui, rearranging files, making main menu

---
+++ scripts/levels/l1_kitchendoor.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+var player_in_range = false
+var player
+
+@onready var prompt = $Sprite2D2
+
+
+func _ready():
+	prompt.visible = false
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		player_in_range = true
+		player = body
+
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_in_range = false
+		player = null
+
+
+func _process(delta):
+	prompt.visible = player_in_range
+
+
+func _input(event):
+	if player_in_range:
+		if event is InputEventMouseButton:
+			if event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+				enter_kitchen()
+
+
+func enter_kitchen():
+	if player:
+
+		if player.collected_ingredients.size() >= 3:
+			get_tree().change_scene_to_file("res://scenes/l1_cookingscene.tscn")
+		else:
+			get_tree().change_scene_to_file("res://scenes/level_1_lose_screen.tscn")
---
+++ scripts/levels/level_2.gd
@@ -0,0 +1,94 @@
+extends Node2D
+
+var required_ingredients = ["pasta", "tomatosauce", "mushrooms"]
+var ingredients_started = false
+
+@onready var arrow = $"UI layer"/arrow
+@onready var player = $player
+
+@onready var tutorialpanel = $"UI layer"/tutorialpanel
+@onready var tutorialtext = $"UI layer"/tutorialpanel/Label
+@onready var tutorialbutton = $"UI layer"/tutorialpanel/Button
+
+@onready var pasta_tick = $UIlayer/ingredientUI/pasta_tick
+@onready var sauce_tick = $UIlayer/ingredientUI/sauce_tick
+@onready var mushrooms_tick = $UIlayer/ingredientUI/mushrooms_tick
+
+@onready var tomatosauce_timer = $"UI layer"/tomatosauce_timer
+@onready var tomatosauce = $ingredients/tomatosauce
+
+func _ready():
+	arrow.visible = false
+	tutorialbutton.visible = true
+	tutorialpanel.visible = true
+	tutorialtext.visible = true
+
+
+func _physics_process(delta):
+	pause()
+	check_ingredients()
+	update_ingredient_UI()
+
+	if tomatosauce:
+		var time_left = tomatosauce.spoil_time - tomatosauce.timer
+		
+		tomatosauce_timer.visible = true
+		
+		if time_left > 0:
+			tomatosauce_timer.text = "Spoils in: " + str(round(time_left))
+		else:
+			tomatosauce_timer.text = "Tomato sauce spoiled"
+	else:
+		tomatosauce_timer.visible = false
+
+
+func pause():
+	if Input.is_action_just_pressed("pause"):
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func check_ingredients():
+	if player.ingredient_failed:
+		arrow.visible = false
+		return
+
+	if player.collected_ingredients.size() >= required_ingredients.size():
+		arrow.visible = true
+	else:
+		arrow.visible = false
+
+
+func update_ingredient_UI():
+
+	if "pasta" in player.collected_ingredients:
+		pasta_label.text = "Pasta ✓"
+	else:
+		pasta_label.text = "Pasta"
+
+
+	if "tomatosauce" in player.collected_ingredients:
+		tomatosauce_label.text = "Tomato sauce ✓"
+	else:
+		tomatosauce_label.text = "Tomato sauce"
+
+
+	if "mushrooms" in player.collected_ingredients:
+		mushrooms_label.text = "Mushrooms ✓"
+	else:
+		mushrooms_label.text = "Mushrooms"
+
+
+func _on_button_pressed() -> void:
+	tutorialbutton.visible = false
+	tutorialpanel.visible = false
+	tutorialtext.visible = false
+	
+	start_ingredients()
+
+func start_ingredients():
+	if ingredients_started:
+		return
+	
+	ingredients_started = true
+	
+	tomatosauce.start_timer()
